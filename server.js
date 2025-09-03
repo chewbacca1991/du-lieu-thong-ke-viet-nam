0989e69c95c4fcde2e7357d67ab45625d6f25429
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-03 20:40:20 +0330

    Fix deprecation warning by updating mongoose.connect options

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connection successful'))
   .catch(err => console.error(err));
 
